fb68bd21f716d2e440743ffc40121d93709020d7
Tomer40 <tomerohana11@gmail.com>
2025-02-20 12:30:08 +0200

    activityintent

---
+++ SignUp.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sunday.com
+{
+    [Activity(Label = "SignUp")]
+    public class SignUp : Activity
+    {
+        TextView AlreadyMember;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.SignUpLayout);
+
+            AlreadyMember = FindViewById<TextView>(Resource.Id.txtAlreadyMember);
+            AlreadyMember.Click += AlreadyMember_Click;
+        }
+
+        private void AlreadyMember_Click(object sender, EventArgs e)
+        {
+            Intent SignUpTomain = new Intent(this, typeof(MainActivity));
+            StartActivity(SignUpTomain);
+        }
+    }
+}
