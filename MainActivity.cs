6da06045eecc216af6e6389c1e35d43c6dc9b8d5
Raniel <ranigonen@gmail.com>
2025-02-20 11:27:46 +0200

    First

--- MainActivity.cs
+++ MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using System;
 
 namespace Sunday.com
 {
@@ -14,6 +15,8 @@ namespace Sunday.com
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            Console.Write("Hellooo");
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
